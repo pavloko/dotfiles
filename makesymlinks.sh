d5a38e6e2f2033c35df449a98928c00b17643e48
pavloko <pavlo.kochubei@hotmail.com>
2016-05-21 12:36:45 +0300

    Modified makesymlinks to support tmux and git

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,7 +7,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc"    # list of files/folders to symlink in homedir
+files="vimrc zshrc gitconfig gitignore tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
